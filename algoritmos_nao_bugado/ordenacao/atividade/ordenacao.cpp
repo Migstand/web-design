1b9a72d7ec1a698855c4e13f11f86bd05a8930e1
Migstand <mrsdahora@gmail.com>
2026-02-04 22:58:23 +0000

    Completar a atividade

--- algoritmos_nao_bugado/ordenacao/atividade/ordenacao.cpp
+++ algoritmos_nao_bugado/ordenacao/atividade/ordenacao.cpp
@@ -1,16 +1,48 @@
 #include "ordenacao.hpp"
 
 /* TODO: Implementar função */
-bool ordenado(int a[],  unsigned int t){
-    return false;
+bool ordenado(int a[],  unsigned int total){
+    int i, qtd = 0;
+    for (i = 1; i<total; i++){
+        if (a[i - 1] <= a[i])
+            qtd++;
+    }
+    return qtd == total - 1;
 }
 
 /* TODO: Implementar função */
-void selecao(int a[], unsigned int t){
+void selecao(int a[], unsigned int taman){
+    int i, j, min, aux;
+    for (i = 0; i < tam-1; i++){
+        min = i;
+        for (j = (i+1); j < tam; j++){
+            if (a[j] < a[min]){
+                min = j;
+            }
+        }
+        if (num[i] != num[min]){
+            aux = num[i];
+            num[i] = num[min];
+            num[min] = aux;
+        }
+    }
 }
 
 /* TODO: Implementar função */
-void insercao(int a[], unsigned int t){
+void insercao(int a[], unsigned int tam){
+    int i, x;
+    for (i = 0; i<tam ; i++){
+        x = a[i];
+        if (i == 0)
+            continue;
+        for (int j = i - 1; j > 0; j--){
+            if (a[j] > x){
+                int z = a[j]
+                a[j] = x;
+                x = 
+            }
+        }
+    }
 }
 
 /* TODO: Implementar função */
